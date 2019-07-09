86a70d64c34317b527a66d834df09d401286c9aa
RedPanda13 <38058703+RedPanda13@users.noreply.github.com>
2019-07-09 10:23:28 -0500

    Project 1
    .

---
+++ src/malish/progect/pkg1/MalishProgect1.java
@@ -0,0 +1,98 @@
+//******************************************************
+//*** Program 1 MadLibs (aka better then Hello World)
+//*** Dakota Malish dm061
+//******************************************************
+//*** 
+//******************************************************
+//*** Program Started: 06/10/2018
+//******************************************************
+//*** 
+//******************************************************
+package malish.progect.pkg1;
+
+/**
+ *
+ * @author RedPanda13 (das me)
+ */
+
+import java.util.*;
+
+public class MalishProgect1 {
+
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("Please enter a Verb:");
+        String Verb1 = input.nextLine();
+        
+        
+        System.out.println("Please enter an Adjective:");
+        String Adjective1 = input.nextLine();
+        
+        System.out.println("Please enter a Plural Noun:");
+        String PlurNoun1 = input.nextLine();
+        
+        
+        System.out.println("Please enter an Adjective:");
+        String Adjective2 = input.nextLine();
+        
+        System.out.println("Please enter a Verb ending in ing:");
+        String Verb2 = input.nextLine();
+        
+                
+        System.out.println("Please enter a verb:");
+        String Verb3 = input.nextLine();
+        
+        System.out.println("Please enter an Number:");
+        String Num1 = input.nextLine();
+        
+        
+        System.out.println("Please enter an Adjective:");
+        String Adjective3 = input.nextLine();
+        
+        System.out.println("Please enter a Plural Noun:");
+        String PlurNoun2 = input.nextLine();
+        
+        
+        System.out.println("Please enter a Plural Noun:");
+        String PlurNoun3 = input.nextLine();
+        
+        System.out.println("Please enter a Plural Noun:");
+        String PlurNoun4 = input.nextLine();
+        
+        System.out.println("Please enter a Type of Relative:");
+        String Rela1 = input.nextLine();
+        
+        
+        System.out.println("Please enter an Adjective:");
+        String Adjective4 = input.nextLine();
+        
+        System.out.println("Please enter an Adjective:");
+        String Adjective5 = input.nextLine();
+        
+        
+        System.out.println("Please enter a Plural Noun:");
+        String PlurNoun5 = input.nextLine();
+        
+        System.out.println("");
+        System.out.println(
+"Come "+Verb1+" at WALMART, where you'll receive " +Adjective1+" discounts on all of yoiur favorite brand name \n"
++PlurNoun1+"." +"Our "+Adjective2+" and "+Verb2+" associates are there to "+Verb3+" you "+Num1+" hours a day." +"Here you will find \n"
++Adjective3+" prices on the "+PlurNoun2+" you need."+PlurNoun3+" for the moms, "+PlurNoun4+" for the kids and all the latest \n"
++ "electronics for the "+Rela1+"."+"So come on down to your "+Adjective4+" "+Adjective5+ " WALMART where"+" the "+PlurNoun5+" come first.");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
